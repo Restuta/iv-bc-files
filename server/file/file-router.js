56f7dd5228f9f3678e104c054c96d39dd030ead4
Restuta <restuta8@gmail.com>
2017-03-14 14:47:01 -0700

    refactored s3 uploading middleware to it’s own file

--- server/file/file-router.js
+++ server/file/file-router.js
@@ -1,20 +1,7 @@
 'use strict'
 
 const router = require('express').Router()  // eslint-disable-line new-cap
-const multer = require('multer')
-const multerS3 = require('multer-s3')
-const s3 = require('../lib/s3')
-
-const uploader = multer({
-	storage: multerS3({
-		s3,
-		bucket: 'coding-challenges',
-		key: (req, file, cb) => {
-			const projectId = req.query.projectId
-			return cb(null, `/projects/${projectId}/${Date.now()}/${file.originalname}`)
-		},
-	}),
-})
+const uploader = require('./s3-tools/uploader-middleware.js')
 
 // This should upload the file, however, you'll still need to add another function after this
 // uploader middleware to actually save a record of this file in Mongo
