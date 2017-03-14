6536c86108e8cebea73840d3ffa555abcdf2ed11
Restuta <restuta8@gmail.com>
2017-03-14 14:03:11 -0700

    adding standard directory structure for file

---
+++ server/file/handler/find-all-for-project.js
@@ -0,0 +1,8 @@
+// 'use strict'
+//
+// const find = require('../query/find')
+//
+// module.exports = (req, res, next) =>
+// 	find()
+// 		.then(projects => res.json(projects))
+// 		.catch(next)
---
+++ server/file/mongo/model.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+	dateModified: {
+		type: Date,
+		default: Date.now,
+	},
+	name: {
+		required: true,
+		type: String,
+	},
+	parentId: {
+		type: String,
+		required: true,
+	},
+	projectId: {
+		type: String,
+		required: true,
+	},
+	size: {
+		type: Number,
+		required: true,
+	},
+	// for simplicity we treat folders as files also, hence type could be file|folder
+	type: {
+		type: String,
+		required: true,
+		uppercase: true,
+		enum: ['FILE', 'FOLDER'],
+	},
+})
+
+module.exports = mongoose.model('File', schema)
